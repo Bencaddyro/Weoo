2d40a0a4eb59947555e9a1edf0a3697a019ad245
BenCaddyro <bencaddyro@protonmail.com>
2024-01-21 15:51:27 +0100

    V0.2.1 Update dependencies Checkpoint post Rally

--- Readme.md
+++ Readme.md
@@ -1,9 +1,10 @@
 # Weoo
-Another StarCitizen GPS engine
+(Yet) Another StarCitizen GPS engine
 
 Heavily inspired by:
 - [Star Citizen Navigation](https://github.com/Valalol/Star-Citizen-Navigation)
 - [Projet Jericho](https://github.com/Graupunkt/project-jericho)
 
-This tool aim to provide a reliable navigation tool to find, save, import, export various Point of Interest in the Stanton system.
-Right now the focus is primarly on planetary navigation fo the upcoming 2024 Daymar Rally
+This tool aim to provide a reliable navigation tool to find, save, import, export various point of interest in the StarCitizen universe.
+
+Please take a look on [issues](https://github.com/Bencaddyro/Weoo/issues) if you look for missing features or bug report
--- build.sh
+++ build.sh
@@ -7,9 +7,17 @@ cargo build --release --target x86_64-pc-windows-gnu
 # Get version
 VERSION=$(grep "^version" Cargo.toml | cut -d "\"" -f 2)
 
+# If main or pre release
+if [ $(git branch --show-current) == "main" ];
+then
+	DEV="";
+else
+	DEV="prerelease"
+fi
+
 # Move binaries
-cp target/release/weoo_nav_tool target/release/weoo_nav_tool_v$VERSION
-cp target/x86_64-pc-windows-gnu/release/weoo_nav_tool.exe target/x86_64-pc-windows-gnu/release/weoo_nav_tool_v$VERSION.exe
+cp target/release/weoo target/release/weoo_$DEVv$VERSION
+cp target/x86_64-pc-windows-gnu/release/weoo.exe target/x86_64-pc-windows-gnu/release/weoo_$DEVv$VERSION.exe
 
 # Zip everything
-zip -j "weoo_nav_tool_v$VERSION.zip" target/release/weoo_nav_tool_v$VERSION target/x86_64-pc-windows-gnu/release/weoo_nav_tool_v$VERSION.exe Database.json CustomPoi.json
+zip -j "weoo_$DEVv$VERSION.zip" target/release/weoo_$DEVv$VERSION target/x86_64-pc-windows-gnu/release/weoo_$DEVv$VERSION.exe Database.json CustomPoi.json
